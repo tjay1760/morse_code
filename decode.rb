fe9aceb981e24c38b48dd4f3839676a25ad2fd77
John Murianki <mcjthiongo@gmail.com>
2023-08-24 03:50:39 +0300

    Add functions for code interpretor

---
+++ decode.rb
@@ -0,0 +1,34 @@
+MORSE_CODE = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+               '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+               '..' => 'I', '.---'  => 'J', '-.-'   => 'K', '.-..'  => 'L',
+               '--' => 'M', '-.'    => 'N', '---'   => 'O', '.--.'  => 'P',
+               '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+               '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+               '-.--' => 'Y', '--..' => 'Z' }.freeze
+
+def decode_char(char)
+  MORSE_CODE[char] || ''
+end
+
+def decode_word(word)
+  decoded_word = ''
+  letters = word.split(' ')
+  letters.each do |letter|
+    decoded_word += decode_char(letter)
+  end
+  decoded_word
+end
+
+def decode_message(message)
+  decoded_message = ''
+  words = message.split('  ')
+  words.each do |word|
+    decoded_message += decode_word(word) + ' '
+  end
+  decoded_message
+end
+
+puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode_message('-- -.--   -. .- -- .')
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
